8d11a88d6402bcd2de008c5d5e474ff7efe83513
hamazsatti0-a11y <hamazsatti0@gmail.com>
2026-05-12 06:27:56 +0000

    01_basics/01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountpassword = 123456
+let accountemail = "hamazo0@gmail.com"
+var accountid = 22466
+accountcity = "Rawalpindi"
+
+accountstate =
+
+  // accountpassword = 22345
+
+ console.log("accountpassword"); 
+
+ console.table({accountemail , accountid , accountpassword , accountcity , accountstate})
+ 
+
+ console.log({
+  accountemail,
+  accountid,
+  accountpassword,
+  accountcity
+})
+
