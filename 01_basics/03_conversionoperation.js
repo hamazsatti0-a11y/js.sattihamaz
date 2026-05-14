5497e14bfe82217baec5f9e3bc677d57e22e28f0
hamazsatti0-a11y <hamazsatti0@gmail.com>
2026-05-14 07:17:46 +0000

    conversion odf datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = "hamaz"
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+
+// 33 => 33
+// 33abc => NaN
+// true => 1 , fasle = 0
+
+let isLoggedIn = "1abc"
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(BooleanIsLoggedIn);
+
+
+// 1 => true , 0 => false 
+// "" => false
+// "hamaz" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
